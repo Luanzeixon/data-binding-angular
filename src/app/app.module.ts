6c2e3a6e3867a4d1cd27c488f9e0511ef8da8f60
Luanzeixon <luananjos765@gmail.com>
2022-01-19 14:16:00 -0300

    Two-Way data-binding (ngModel)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
+import { MeuFormModule } from './meu-form/meu-form.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataBindingComponent } from './data-binding/data-binding.component';
@@ -12,7 +13,9 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, //para usar o ngModel, $event...
+    MeuFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
