b750bc0656e35abcd27e72b7088de13454b3dc49
Luanzeixon <luananjos765@gmail.com>
2022-01-19 13:03:20 -0300

    Event Binding#

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -3,7 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  //styleUrls: ['./data-binding.component.css']
+  //styles, mesma coisa que o templates usando o ''
+  styles: [
+    `
+    .highlight{
+    background-color: yellow;
+    font-weight: bold;
+  }
+
+    `
+  ]
+
 })
 export class DataBindingComponent implements OnInit {
 
@@ -12,12 +23,30 @@ export class DataBindingComponent implements OnInit {
 
   cursoAngular: boolean = true;
 
+  valorAtual: String = '';
+  valorSalvo: String = '';
+
+  isMouseOver:boolean = false;
+
   getValor(){
     return 1; // metodo para ser interpolizada
   }
 
   getCurtirCurso(){
     return true;
+  }
+
+  botaoClicado(){
+    alert('botao clicado');
+  }
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = ((<HTMLInputElement>evento.target).value); //Evento.taget é do tipo htmlinputelemnet ai precisa fazer isso pra acessar as informações, lembrando que target era onde tava valor no log da pagina
+  }
+  valorSalvar(valor:any){
+    this.valorSalvo = valor;
+  }
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver; //negando o valor, se tiver true vai falso e se falso true
   }
 
   constructor() { }
